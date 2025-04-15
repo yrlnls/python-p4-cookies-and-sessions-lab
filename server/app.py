2a368caf0eda8a6d4106b882de7ea737a9829d22
tillenotuoma <totuoma27@gmail.com>
2025-04-15 23:12:24 +0300

    passed

--- server/app.py
+++ server/app.py
@@ -3,7 +3,7 @@
 from flask import Flask, make_response, jsonify, session
 from flask_migrate import Migrate
 
-from models import db, Article, User
+from models import db, Article
 
 app = Flask(__name__)
 app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
@@ -20,15 +20,16 @@ def clear_session():
     session['page_views'] = 0
     return {'message': '200: Successfully cleared session data.'}, 200
 
-@app.route('/articles')
-def index_articles():
-
-    pass
-
 @app.route('/articles/<int:id>')
 def show_article(id):
-
-    pass
+    session['page_views'] = session.get('page_views', 0)
+    session['page_views'] += 1
+    
+    if session['page_views'] <= 3:
+        article = Article.query.get_or_404(id)
+        return jsonify(article.to_dict())
+    else:
+        return jsonify({'message': 'Maximum pageview limit reached'}), 401
 
 if __name__ == '__main__':
     app.run(port=5555)
